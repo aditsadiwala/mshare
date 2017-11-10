91826e2fdb6ba86eb7e38279ee5b494599587988
aditsadiwala97 <asadiwala_97@hotmail.com>
2017-11-10 19:47:22 +0530

    First commit

---
+++ music/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import userprofile,music,comments
+# Register your models here.
+admin.site.register(userprofile)
+admin.site.register(music)
+admin.site.register(comments)
+
+
